8ea31e96d271f0be4f046ba96b737062d4fc4c5b
Leo <leo.tsai.2010@gmail.com>
2014-01-03 16:13:02 +0800

    updated code  format

--- demos/user management/www/js/demo/02.UserService.js
+++ demos/user management/www/js/demo/02.UserService.js
@@ -2,21 +2,21 @@
 
 };
 
-UserService.prototype = {    
-    getAll: function (callback) {
+UserService.prototype = {
+    getAll: function(callback) {
         demo.db.getInstance().users.toArray(callback);
     },
-    add:function(user, callback) {
+    add: function(user, callback) {
         var db = demo.db.getInstance();
         user.lastUpdatedTime = null;
         db.users.add(user);
         db.saveChanges(callback);
     },
-    deleteUser:function(id, callback) {
+    deleteUser: function(id, callback) {
         var db = demo.db.getInstance();
         db.users.removeByWhere("id=" + id, callback);
     },
-    update:function(user, callback) {
+    update: function(user, callback) {
         var db = demo.db.getInstance();
         db.users.where("id=" + user.id).firstOrDefault(function(dbUser) {
             dbUser.updateFrom(user);
@@ -27,7 +27,7 @@ UserService.prototype = {
             });
         });
     },
-    get:function(id, callback) {
+    get: function(id, callback) {
         demo.db.getInstance().users.firstOrDefault(callback, "id=" + id);
     }
 };
